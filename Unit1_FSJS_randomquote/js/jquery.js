518f09f5939273ac607c2b7813956e1f1374ef49
Eliza Dascal <elizadascal@gmail.com>
2017-02-07 16:39:56 +0000

    u1 random quote script

---
+++ Unit1_FSJS_randomquote/js/jquery.js
@@ -0,0 +1,10 @@
+//Creates nice fade in effect
+function main() {
+	$('#quote-box').hide();
+	$('#quote-box').fadeIn(1000);
+	//$('#loadQuote').on('click', function(){
+		//$('#quote-box').hide();
+		//$('#quote-box').fadeIn(1000);
+	//});
+}
+$(document).ready(main);
